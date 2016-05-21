ec1a20ed5951e3410e29f8cc89efec29c045d88e
huhuics <hhuics@163.com>
2016-05-21 15:50:52 +0800

    集成mybatis-generator

---
+++ mechmng-dao/src/main/java/org/mechmng/dao/DepartmentMapper.java
@@ -0,0 +1,17 @@
+package org.mechmng.dao;
+
+import org.mechmng.dao.domain.Department;
+
+public interface DepartmentMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Department record);
+
+    int insertSelective(Department record);
+
+    Department selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Department record);
+
+    int updateByPrimaryKey(Department record);
+}
---
+++ mechmng-dao/src/main/java/org/mechmng/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package org.mechmng.dao;
+
+import org.mechmng.dao.domain.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
